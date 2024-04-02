5d0bda102b9d76c341c33f31bb4ec8c7bc21aefb
Henrique Chrispim <henrique.chrispim@mettainnovations.com.br>
2024-04-02 11:49:52 -0300

    Ajustes

--- Lab2/geraVetor.c
+++ Lab2/geraVetor.c
@@ -6,23 +6,33 @@ void outputArray(long int size);
 
 int main(int argc, char* argv[])
 {
+    long int arraySize;
     if(argc != 2)
     {
-        printf("Erro no argumento de linha de comando\n");
+        printf("Utilização correta: %s <tamanho_vetor>\n", argv[0]);
         return 1;
     }
 
     srand(time(NULL));
 
-    outputArray(atoi(argv[1]));
+    arraySize = (long int)atoi(argv[1]);
+
+    if(arraySize <= 0)
+    {
+        printf("Tamanho do vetor deve ser um inteiro positivo.\n");
+        return 2;
+    }
+
+    outputArray(arraySize);
+    printf("\n");
 
     return 0;
 }
 
 void outputArray(long int size)
 {
-    int i;
+    long int i;
     printf("%ld ", size);
     for(i = 0; i < size; i++)
-        printf("%f ", (float)rand()/(float)(RAND_MAX/100));
+        printf("%f ", (float)rand() * 100 / RAND_MAX);
 }
